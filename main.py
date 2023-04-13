3492e3fa5073a106b7288fff65f321ec4677d4ad
Sangbaek Lee <sangbaek.lee@anl.gov>
2023-04-13 17:36:32 -0500

    Upload the working generator

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from km15gen import bhdvcs
+from km15gen import genOneEvent
 import argparse
 import numpy as np
+import time
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Get args",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("-Ed", "--Ed", type = float, default = 10.604)
   parser.add_argument("-trig", "--trig", type = int, default = 1)
+  parser.add_argument("-fname", "--fname", type = str, default = "km15gen")
   parser.add_argument("-xBmin", "--xBmin", type = float, default = 0.05)
   parser.add_argument("-xBmax", "--xBmax", type = float, default = 0.75)
   parser.add_argument("-Q2min", "--Q2min", type = float, default = 0.9)
@@ -14,7 +16,7 @@ if __name__ == '__main__':
   parser.add_argument("-tmax", "--tmax", type = float, default = 1.79)
   parser.add_argument("-ymin", "--ymin", type = float, default = 0.19)
   parser.add_argument("-ymax", "--ymax", type = float, default = 0.85)
-  parser.add_argument("-find_max", "--find_max", action = 'store_true')
+  parser.add_argument("-w2min", "--w2min", type = float, default = 3.61)
   args = parser.parse_args()
 
 
@@ -28,12 +30,24 @@ if __name__ == '__main__':
   tmax  = args.tmax
   ymin  = args.ymin
   ymax  = args.ymax
+  w2min  = args.w2min
 
-  if args.find_max:
-    xs_array = []
-    while len(xs_array) < args.trig:
-      xs = bhdvcs(xBmin, xBmax, Q2min, Q2max, tmin, tmax, ymin, ymax, xs_only = 1)
-      if xs:
-        xs_array.append(xs)
 
-    print(np.max(xs_array))
+  now = time.time()
+  trig = args.trig
+  num  = 0
+  while num <trig:
+    result   = genOneEvent(xBmin, xBmax, Q2min, Q2max, tmin, tmax, ymin, ymax, w2min, 0,  rad = 1, filename = args.fname)
+    num      = num + result
+  later = time.time()
+  print(later-now)
+
+
+  # if args.find_max:
+  #   xs_array = []
+  #   while len(xs_array) < args.trig:
+  #     xs = bhdvcs(xBmin, xBmax, Q2min, Q2max, tmin, tmax, ymin, ymax, xs_only = 1)
+  #     if xs:
+  #       xs_array.append(xs)
+
+  #   print(np.max(xs_array))
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
+import glob
 
 
 
@@ -9,10 +10,8 @@ extensions = [
     Extension("processing_module", sources=["processing_module.pyx"], include_dirs=[numpy.get_include()], extra_compile_args=["-O3"], language="c++")
 ]
 
-
 setup(
-    name='km15gen',
-    ext_modules=cythonize("km15gen.pyx"),
-    zip_safe=False,
+    name='km15gen_c',
+    ext_modules=cythonize(["km15gen.pyx"] , language_level = 3, annotate=True),
+    zip_safe=False
 )
-
