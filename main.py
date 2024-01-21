a8b2b8fe5a357d9c7a050ea7da4e453878f0449b
Sangbaek <sangbaek.lee@anl.gov>
2024-01-21 16:26:59 -0500

    Save the current version for the ifarm

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def main(args):
       "--q2", "{:.3f}".format(Q2min), "{:.3f}".format(Q2max),
       "--t", "{:.3f}".format(tmin), "{:.3f}".format(tmax),
       "--gpd", "101", "--y", "{:.3f}".format(ymin), "{:.3f}".format(ymax), "--w", "{:.3f}".format(w2min),
-      "--raster", "0.025", "--writef", "2", "--globalfit", "--ycol", "0.0005"]
+      "--raster", "0.025", "--writef", "2", "--globalfit", "--ycol", "0.0005", "--weight"]
     if rad:
       dvcsgen_commands.extend(["--radgen", "--vv2cut", "0.6", "--delta", "0.1", "--radstable"])
     dvcsgen_commands.extend(["--bh", "1"])
@@ -97,7 +97,7 @@ def main(args):
       "--q2", "{:.3f}".format(Q2min), "{:.3f}".format(Q2max),
       "--t", "{:.3f}".format(tmin), "{:.3f}".format(tmax),
       "--gpd", "101", "--y", "{:.3f}".format(ymin), "{:.3f}".format(ymax), "--w", "{:.3f}".format(w2min),
-      "--raster", "0.025", "--writef", "2", "--globalfit", "--ycol", "0.0005"]
+      "--raster", "0.025", "--writef", "2", "--globalfit", "--ycol", "0.0005", "--weight"]
     if rad:
       dvcsgen_commands.extend(["--radgen", "--vv2cut", "0.6", "--delta", "0.1", "--radstable"])
     dvcsgen_commands.extend(["--bh", "3"])
